953eaf70865bf50b016fa311ef1055344305db0a
sapana subedi <subedisapana10@gmail.com>
2019-05-26 18:31:12 +0545

    homepage created

---
+++ resources/views/home.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Karkhana</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great" rel="stylesheet">
+    <meta name="keywords" content="footer, address, phone, icons" />
+
+    <link rel="stylesheet" href="css/demo.css">
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    
+
+    <link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="../css/animate.css">
+    <link rel="stylesheet" type="text/css" href="../css/footer.css">
+    <link rel="stylesheet" href="../css/magnific-popup.css">
+
+    <link rel="stylesheet" href="../css/aos.css">
+
+    <link rel="stylesheet" href="../css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="../css/flaticon.css">
+    <link rel="stylesheet" href="../css/icomoon.css">
+    <link rel="stylesheet" href="../css/home.css">
+  </head>
+<body>
+
+    <header>
+        <nav>
+          
+        <div class="main" id="resnav">
+         <!-- <div class="logo">
+            <img src="/images/logo.jpg">-->
+                 <ul>
+            <li><a href="/"><font color = "black">Home</font></a></li>
+            <li><a href="#"><font color = "black">Courses</font></a></li>
+            <li><a href="#"><font color = "black">About</font></a></li>
+            <li><a href="#" class="rectangle" ><font color = "black">Register for free</font></a></li>
+                <a href="javascript:void(0);" class="icon" onclick="menubtn()">
+                            <i class="fa fa-bars"></i>
+                        </a>
+
+           </ul>
+            </div>
+        </div>
+          </nav>
+        </header>
+
+
+    <section class="ftco-services ftco-no-pb">
+      <div class="container-wrap">
+        <div class="row no-gutters">
+
+          <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-primary">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+                <span class="flaticon-teacher"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Computational Thinking</h3>
+                <p>Computational thinking describes the mental activity in formulating a problem to admit a computational solution. It allows one to understand how to interact with computers.</p>
+              </div>
+            </div>      
+          </div>
+
+          
+    
+          <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-fifth">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+                <span class="flaticon-books"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Design &amp; Visual Communication</h3>
+                <p>Human brains are hardwired to process images quickly, and that means visuals work better than audio and words, when trying to get your message across.</p>
+              </div>
+            </div>      
+          </div>
+
+          <div class="col-md-3 d-flex services align-self-stretch pb-4 px-4 ftco-animate bg-quarternary">
+            <div class="media block-6 d-block text-center">
+              <div class="icon d-flex justify-content-center align-items-center">
+                <span class="flaticon-diploma"></span>
+              </div>
+              <div class="media-body p-2 mt-3">
+                <h3 class="heading">Digital Citizenship</h3>
+                <p>Addressing the 21st century skills of digital citizenship is important; to help students to learn, communicate and collaborate safely online. </p>
+              </div>
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
+
+ <section class="bgimg"> 
+  <div class="signin">
+
+    <p class="sign" align="center">Sign in</p>
+    <form class="form1">
+      <input class="un " type="text" align="center" placeholder="Username">
+      <input class="pass" type="password" align="center" placeholder="Password">
+      <!--<a class="submit" align="center">Sign in</a>-->
+     <button  class="button sbutton" type="button" onclick="alert('Hello world!')">Sign in</button>
+       <div>
+      <p class="forgot" align="center"><a href="#">Forgot Password?</a></p>
+      <p class="Register" align="center"><a href="#">Register Now!</a></p>              
+    </div>
+</form>
+</div>
+</section>
+
+
+<footer class="footer-distributed">
+
+      <div class="footer-left">
+
+        <p class="footer-links">
+          <a href="#">Home</a>
+          ·
+          <a href="#">About</a>
+          ·
+          <a href="#">Faq</a>
+          ·
+          <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Karkhana &copy; 2013</p>
+      </div>
+
+      <div class="footer-center">
+
+        <div>
+          <i class="fa fa-map-marker"></i>
+          <p><span>Karkhana</span> Gyaneshwor, Kathmandu</p>
+        </div>
+
+        <div>
+          <i class="fa fa-phone"></i>
+          <p>+977-01-4429963 / +977-01-4412624
+</p>
+        </div>
+
+        <div>
+          <i class="fa fa-envelope"></i>
+          <p><a href="mailto:info@karkhana.asia">support our orginazation</a></p>
+        </div>
+
+      </div>
+
+      <div class="footer-right">
+
+            <p class="footer-company-about">
+              <span>About the organization</span>
+          <p>
+           Karkhana is an education company and makerspace with a unique approach to learning.
+          </p>
+
+          <div class="footer-icons">
+
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-linkedin"></i></a>
+            <a href="#"><i class="fa fa-github"></i></a>
+
+          </div>
+
+
+            </div>
+
+    
+  </footer>
+
+
+ 
+    </body>
+  
+  <script src="../js/jquery.min.js"></script>
+  <script src="../js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="../js/popper.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/jquery.easing.1.3.js"></script>
+  <script src="../js/jquery.waypoints.min.js"></script>
+  <script src="../js/jquery.stellar.min.js"></script>
+  <script src="../js/owl.carousel.min.js"></script>
+  <script src="../js/jquery.magnific-popup.min.js"></script>
+  <script src="../js/aos.js"></script>
+  <script src="../js/jquery.animateNumber.min.js"></script>
+  <script src="../js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="../js/google-map.js"></script>
+  <script src="../js/main.js"></script>
+    
+  </body>
+</html>
