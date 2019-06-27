9df2ed738203e768cacfe6cd57f6d75de6b9650b
sapana subedi <subedisapana10@gmail.com>
2019-06-27 23:11:48 +0545

    MVC for Student's registration

--- app/suser.php
+++ app/suser.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Suser extends Model
 {
-    //
+     use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'email', 'password' 'confirm_password,school
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
+
 }
